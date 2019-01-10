e90614f2e30770126faee4302537bb56365daa6d
Philip <phr.debruin@gmail.com>
2019-01-10 14:22:49 +0100

    spelfotos toegevoegd

--- resources/views/spelkeuze.blade.php
+++ resources/views/spelkeuze.blade.php
@@ -12,7 +12,7 @@
         @foreach ($spelletjes as $spelletje)
             <div class="card-body div-spel">
                 <button type="button" class="knop-spel" onclick="document.location='/spel{{ $spelletje->id }}'">
-                    <img class="img-spel" src="{{ asset('afbeeldingen/foto' . $spelletje->id . '.jpg') }}" alt="foto{{ $spelletje->id }}">
+                    <img class="img-spel" src="{{ asset('afbeeldingen/spellen/spel' . $spelletje->id . '.png') }}" alt="foto{{ $spelletje->id }}">
                 </button>
                 <p class="label-spel">{{ $spelletje->spel_naam }}</p>
             </div>
