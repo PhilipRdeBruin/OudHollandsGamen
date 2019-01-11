f3ba6a385734402102d7a50b26318f293f02ed63
jacomijnsteen <jacomijnsteen@gmail.com>
2019-01-11 09:39:13 +0100

    Merged route conflict

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,15 @@ Route::get('/keuze', function () {
     return view('keuze');
 });
 
-<<<<<<< HEAD
 Route::get('/keuzevrienduitnodiging', function () {
     return view('keuzevrienduitnodiging');
 });
 
 
-=======
 Route::get('/spelkeuze', function () {
     return view('spelkeuze');
 });
 
->>>>>>> 76cf5e8dfa26f019ccc0bee892e68a9f5f90a500
 Auth::routes();
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
